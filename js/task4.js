b4ee1afb34642e80a42a3522c2c57f503ba67c33
GuruVII <guruvii@gmail.com>
2017-02-24 18:58:18 +0100

    task 4 complete

--- js/task4.js
+++ js/task4.js
@@ -1,1 +1,11 @@
-task4.js
+var object = {
+                alert : function(txt){
+                  alert(txt);
+                  return this;
+                },
+                confirm : function(txt){
+                  confirm(txt);
+                  return this;
+                }
+              }
+object.alert("Warning: you are about to delete this item!").confirm("Are you sure?");
