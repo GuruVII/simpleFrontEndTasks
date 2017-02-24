4a8d6fcdeb5b52dbe20731dda9d81bee26eaa525
GuruVII <guruvii@gmail.com>
2017-02-24 19:24:50 +0100

    5th task complete

---
+++ js/task5.js
@@ -0,0 +1,22 @@
+$( document ).ready(function() {
+
+	document.getElementById("element1").addEventListener("mousemove", function(event) {
+		event.stopPropagation();
+		$("#element1").css("backgroundColor", "blue");
+	    $("#element3").css("backgroundColor", "white");
+	    $("#element2").css("backgroundColor", "white");
+	    
+	});
+	document.getElementById("element2").addEventListener("mousemove", function(event) {
+		event.stopPropagation();
+	    $("#element2").css("backgroundColor", "blue");
+	    $("#element1").css("backgroundColor", "white");
+	    $("#element3").css("backgroundColor", "white");
+	});
+	document.getElementById("element3").addEventListener("mousemove", function(event) {
+		event.stopPropagation();
+	    $("#element3").css("backgroundColor", "blue");
+	    $("#element1").css("backgroundColor", "white");
+	    $("#element2").css("backgroundColor", "white");
+	});
+});
