e8f88dcf19da0ba59cb57930fdef02772973a7dc
GuruVII <guruvii@gmail.com>
2017-02-24 19:32:34 +0100

    slight change to task 5

--- js/task5.js
+++ js/task5.js
@@ -1,5 +1,4 @@
 $( document ).ready(function() {
-
 	document.getElementById("element1").addEventListener("mousemove", function(event) {
 		event.stopPropagation();
 		$("#element1").css("backgroundColor", "blue");
@@ -16,6 +15,13 @@ $( document ).ready(function() {
 	document.getElementById("element3").addEventListener("mousemove", function(event) {
 		event.stopPropagation();
 	    $("#element3").css("backgroundColor", "blue");
+	    $("#element1").css("backgroundColor", "white");
+	    $("#element2").css("backgroundColor", "white");
+	});
+	
+	document.getElementsByClassName("container")[0].addEventListener("mousemove", function(event) {
+		event.stopPropagation();
+	    $("#element3").css("backgroundColor", "white");
 	    $("#element1").css("backgroundColor", "white");
 	    $("#element2").css("backgroundColor", "white");
 	});
