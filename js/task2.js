2aa48ee742d8037c769d3fcb67ebcfe5b5e853a0
GuruVII <guruvii@gmail.com>
2017-02-24 18:25:42 +0100

    task 3 finished

--- js/task2.js
+++ js/task2.js
@@ -1,11 +1,11 @@
 var index, 
 length = 20; 
 	
-	for (index=0;index < length ; index++) {
+	for (index=0;index < this.length ; index++) {
 	   (function(index) {
 	       setTimeout(function(){
 	       	console.log(index);
-	       	
+
 	       }, 200 * (index+1) );
 	   })(index);
 }
