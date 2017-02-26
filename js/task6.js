531e6cdd8e8d3b3b5742313c62e9ab9918153022
Guru VII <Guru VII>
2017-02-26 01:47:50 +0100

    making progress on the ajax simulation

--- js/task6.js
+++ js/task6.js
@@ -1,28 +1,74 @@
 (function($) {
-var app = angular.module('app', ['infinite-scroll','ngMockE2E']);
+var app = angular.module('app', ['infinite-scroll','ngMockE2E', 'ngResource']);
     
     app.run(function($httpBackend) {
-      var phones = [
-          {name: 'phone1'}, 
-          {name: 'phone2'}
+        var step = 0;
+      var allPhones = [
+          {    id: 0,
+              name: 'phone1'}, 
+          {     id: 1, 
+              name: 'phone2'},
+          {     id: 2, 
+              name: 'phone3'},
+          {     id: 3, 
+              name: 'phone4'},
+          {     id: 4, 
+              name: 'phone5'},
+          {     id: 5, 
+              name: 'phone6'},
+          {     id: 6, 
+              name: 'phone7'},
+          {     id: 7, 
+              name: 'phone8'},         
+          {     id: 8, 
+              name: 'phone9'},
+          {     id: 9, 
+              name: 'phone10'},
+          {     id: 10, 
+              name: 'phone11'},
+          {     id: 11, 
+              name: 'phone12'},
+          {     id: 12, 
+              name: 'phone13'},
+          {     id: 13, 
+              name: 'phone14'},
+          {     id: 14, 
+              name: 'phone15'},
+          {     id: 15, 
+              name: 'phone16'},
+          {     id: 16, 
+              name: 'phone17'},
+          
       ];
 
       // returns the current list of phones
-      $httpBackend.whenGET('/phones').respond(phones);
+        $httpBackend.whenGET(/\/phones\/(\d+)/, undefined, ['id']).respond(function(method, url, data, headers, params) {
+            var phones =[];
+            
+          for (var i = step; i < params.id; i++){
+              phones.push(allPhones[i]);
+          };
+            step = params.id;
+            console.log(step);
+          if (phones == null) {
+            return [404, undefined, {}];
+          }
 
+          return [200, phones, {}];
+});
     });
-
+    
     angular.module("app").factory("task6Factory", function($http) {
         var task6Factory = {
             partyList: function() {
-                var promise = $http.get('/phones')
+                var promise = $http.get('/phones/8')
                     .then(function(response) {
                         //First function handles success
                         console.log("test response")
                         return response.data;
                     }, function(response) {
                         //Second function handles error
-                        console.log("error in acquring phone list")
+                        console.log("error acquiring product list")
                     });
                 return promise
             }
@@ -34,9 +80,10 @@ var app = angular.module('app', ['infinite-scroll','ngMockE2E']);
         $scope.masterArray = [];
         task6Factory.partyList().then(function(successResponse) {
                     if (successResponse == undefined) {
-                        console.log("undifines -.-")
+                        console.log("Get failed")
                     };
                     $scope.masterArray = successResponse;
+            console.log($scope.masterArray);
         });
 }]);
 })(jQuery); // end of jQuery name space
